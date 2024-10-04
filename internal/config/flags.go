61da19814a1def5f876a330a7dc0be6f589899be
Makovey Vladislav <vladislav.makovey@raiffeisen.ru>
2024-10-04 15:12:38 +0300

    prevent redefine flags

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -1,21 +1,43 @@
 package config
 
-import "flag"
+import (
+	"flag"
+)
+
+const (
+	flagAddr    = "a"
+	flagBaseURL = "b"
+)
 
 type flagsValue struct {
 	addr            string
 	baseReturnedURL string
 }
 
-func (v *flagsValue) parseFlags() {
-	flag.StringVar(&v.addr, "a", "localhost:8080", "the address to listen on for HTTP requests")
-	flag.StringVar(&v.baseReturnedURL, "b", "http://localhost:8080", "base url returned in response when url is shorted")
+func (v *flagsValue) parseFlagsIfNeeded() {
+	if flag.Lookup(flagAddr) == nil {
+		flag.StringVar(&v.addr, flagAddr, "localhost:8080", "the address to listen on for HTTP requests")
+	}
+
+	if flag.Lookup(flagBaseURL) == nil {
+		flag.StringVar(&v.baseReturnedURL, flagBaseURL, "http://localhost:8080", "base url returned in response when url is shorted")
+	}
+
 	flag.Parse()
+}
+
+func (v *flagsValue) getFlagsValue() {
+	v.addr = flag.Lookup(flagAddr).Value.(flag.Getter).Get().(string)
+	v.baseReturnedURL = flag.Lookup(flagBaseURL).Value.(flag.Getter).Get().(string)
 }
 
 func newFlagsValue() flagsValue {
 	f := flagsValue{}
-	f.parseFlags()
+	f.parseFlagsIfNeeded()
+
+	if f.addr == "" || f.baseReturnedURL == "" {
+		f.getFlagsValue()
+	}
 
 	return f
 }
